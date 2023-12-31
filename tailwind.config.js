5f0dc7a9f9c9ec77759d96b873519796267bff7f
Urvish Patel <urvish.xyz@gmail.com>
2023-12-31 22:03:20 +0530

    projects added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: [],
   theme: {
     extend: {},
+    colors: {
+      tahiti: {
+        light: "#67e8f9",
+        DEFAULT: "#434343",
+        dark: "#0e7490",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
