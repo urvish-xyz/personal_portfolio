d617de5662bcc3dfab74e674099d5f2630340e0a
Urvish Patel <urvish.xyz@gmail.com>
2024-01-11 10:23:32 +0530

    Url changes

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function hideMenu() {
   // Add code to hide the menu, for example:
   document.getElementById("openSidebarMenu").checked = false;
 }
+
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+  return false; // Prevents the default behavior of the anchor tag
+}
